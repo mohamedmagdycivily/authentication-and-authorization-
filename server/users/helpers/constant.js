dc1ce5543eda4022e5d47a0e1fab05d082ba3542
mohamed hanafy <mohamed.hanafy@bosta.co>
2022-04-10 01:20:13 +0200

    feat: adding roles to users module

--- server/users/helpers/constant.js
+++ server/users/helpers/constant.js
@@ -1,5 +1,8 @@
 export const USER__REGISTER = 'user:register';
 export const USER__LOGIN = 'user:login';
+export const USER__UPDATE_PERSONAL_INFO = 'user:updatePersonalInfo';
+
 // ROLES
 export const ADMIN = 'ADMIN';
-export const ROLES = [ADMIN]
+export const SUPER_ADMIN = 'SUPER_ADMIN'
+export const ROLES = [ADMIN, SUPER_ADMIN]
