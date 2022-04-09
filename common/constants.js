dd8b3a4aee6fedb3f764a5350716d353f339bc25
mohamed hanafy <mohamed.hanafy@bosta.co>
2022-04-09 19:43:28 +0200

    feat: adding constants to project

---
+++ common/constants.js
@@ -0,0 +1,12 @@
+export const BASE_URL = '/api/v0';
+export const SALT_ROUNDS = 12;
+
+export const MESSAGES = {
+    CREATED: 'created successfully.',
+    DONE_SUCCESSFULLY: 'Done successfully.',
+}
+export const errorCodes = {
+  ILLEGAL_ARGUMENT_EXCEPTION: { message: 'Illegal argument exception.', code: 1003 },
+  EMAIL_USED_BEFORE: { message: 'The email you provided is used before.', code: 1012 },
+
+}
