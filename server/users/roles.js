a39706fdeecde1170f227665ce8171eed27e6266
mohamed hanafy <mohamed.hanafy@bosta.co>
2022-04-10 01:15:48 +0200

    feat: adding roles to users module

---
+++ server/users/roles.js
@@ -0,0 +1,12 @@
+import {
+    USER__UPDATE_PERSONAL_INFO,
+    ADMIN, SUPER_ADMIN,
+} from './helpers/constant';
+
+export default {
+[USER__UPDATE_PERSONAL_INFO]: [
+    ADMIN,
+    SUPER_ADMIN,
+],
+};
+  
