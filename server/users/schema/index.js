4d4ba8a4a50701c17893d562089effc1666f4da9
mohamed hanafy <mohamed.hanafy@bosta.co>
2022-04-09 19:46:02 +0200

    feat: user schema

---
+++ server/users/schema/index.js
@@ -0,0 +1,74 @@
+import mongoose from 'mongoose';
+import nanoid from '../../../common/config/nanoid';
+import {
+  ROLES
+} from '../helpers/constant';
+const { Schema } = mongoose;
+const UserSchema = new Schema(
+    {
+      _id: {
+        type: String,
+        default: () => nanoid(),
+      },
+      email: {
+        type: {
+          address: {
+            type: String,
+            lowercase: true,
+            trim: true,
+          },
+          verified: { type: Boolean, default: false },
+        },
+      },
+      emailToken: {
+        type: String,
+      },
+      emailTokenExpiration: {
+        type: String,
+      },
+      profile: {
+        type: {
+          firstName: {
+            type: String,
+            required: true,
+          },
+          lastName: {
+            type: String,
+            required: true,
+          },
+        },
+      },
+      roles: {
+        type: [{ type: String }],
+        enum: ROLES,
+        default: [],
+      },
+      hashedPassword: {
+        type: String,
+      },
+      group: {
+        type: {
+          _id: {
+            type: String,
+          },
+          name: {
+            type: String,
+          },
+          code: {
+            type: Number,
+          },
+        },
+      },
+      lastSessionResetDate: {
+        type: Date,
+        required: false,
+        default: new Date(),
+      },
+
+    },
+    { collection: 'users', timestamps: true },
+  );
+  
+  const User = mongoose.model('users', UserSchema);
+  
+  export default User;
